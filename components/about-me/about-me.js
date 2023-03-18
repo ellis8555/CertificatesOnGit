6f234c10c9d91a81af9e7df2302ec67a974446be
ellis <lsp1@hotmail.com>
2023-03-17 20:55:55 -0600

    src code

---
+++ components/about-me/about-me.js
@@ -0,0 +1,13 @@
+import { createRouteContainer } from "../../utilities/routes/createDivRoutes";
+import { aboutMeFRAG } from "./about-me-FRAG";
+
+const aboutMe = `
+<div class="container pt-5 px-5">
+  <p class="display-5 text-center pb-5">About me</p>
+  <div class="rounded">
+    <div id="aboutMe">${aboutMeFRAG}</div>
+  </div>
+</div>`;
+
+const aboutMeElement = createRouteContainer(aboutMe, "about-me");
+export { aboutMeElement };
---
+++ components/elements.js
@@ -0,0 +1,16 @@
+import { aboutMeElement } from "./about-me/about-me";
+import { underConstructionElement } from "./construction/under-construction";
+import { navbarElement } from "./navbar/navbar";
+import { projectsElement } from "./projects/projects";
+import { certificatesElement } from "./certificates/certificates";
+import { todosElement } from "./Todos/todos";
+import { hobbiesElement } from "./hobbies/hobbies";
+export {
+  aboutMeElement,
+  underConstructionElement,
+  navbarElement,
+  projectsElement,
+  certificatesElement,
+  todosElement,
+  hobbiesElement,
+};
