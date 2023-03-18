5dd9d389aa8cf955d6a616fc6122e5a7766767d6
ellis <lsp1@hotmail.com>
2023-03-18 09:08:30 -0600

    display webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg|ico)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/i,
+        loader: "svg-inline-loader",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/template.html"),
+      filename: "index.html",
+      favicon: path.resolve(__dirname, "./src/images/favicon/favicon.ico"),
+    }),
+  ],
+};
