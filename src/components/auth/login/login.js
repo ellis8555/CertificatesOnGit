640956c0ea3b6fdf9e4859d3cf7b986642abfb59
ellis <lsp1@hotmail.com>
2023-03-31 19:31:22 -0600

    v5.3

--- src/components/auth/login/login.js
+++ src/components/auth/login/login.js
@@ -1,12 +1,14 @@
 import { createRouteContainer } from "../../../utilities/allUtilities";
+import { loginFormElement } from "./loginFRAGS/login-form-FRAG";
 
 const login = `
-<div class="px-sm-5">
-<p class="display-5 text-center pb-5">Login</p>
-<div class="rounded">
-    <div id="loginContent">
-        <h1 class="text-warning text-center">login under construction</h1>
-    </div>
+<div class="px-sm-5 py-3 mt-5">
+<p class="display-5 text-center">Login</p>
+<div class="text-center mt-5" id="loginResponseIndicator"></div>
+<div class="row d-flex justify-content-center">
+<div class="col-sm-12 col-md-9 col-lg-7 col-xl-6 rounded d-flex justify-content-center mt-3">
+${loginFormElement.innerHTML}
+</div>
 </div>
 </div>
 `;
